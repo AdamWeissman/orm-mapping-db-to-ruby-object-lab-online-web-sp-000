3f74f14487da705d7eb0b5374b43f7192cd478d4
Adam Weissman <adam@comedytragedyepic.com>
2019-11-16 08:53:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,6 +93,17 @@ class Student
     end
   end
   
+  def self.all_students_in_grade_X(?)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+ 
+    make_a_new_db(sql)
+  end   
+
+  
   def self.first_student_in_grade_10
     first_X_students_in_grade_10(5).first
   end
